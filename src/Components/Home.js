11203d39a79274f646870bab2f2df845066ba260
Brandy McArthur <bdm.mcarthur@gmail.com>
2020-01-04 18:25:15 -0500

    Header and navbar done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import background1 from "../images/carousel1.jpg";
+import background2 from "../images/carousel2.jpg";
+import background3 from "../images/carousel3.jpg";
 
 export default class Home extends Component {
   constructor(props) {
@@ -8,8 +11,67 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
-        <h1>This is Home.</h1>
+      <div class="home">
+        <div class="title-container">
+          <h1 class="title-text">Ballywole</h1>
+          <p class="title-description font-italic text-center mb-5">
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim.
+          </p>
+          <button
+            type="button"
+            class="d-block mx-auto title-btn btn btn-light btn-lg"
+          >
+            Explore
+          </button>
+        </div>
+        <div
+          id="carouselExampleControls"
+          class="carousel slide"
+          data-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                class="d-block w-100"
+                src={background1}
+                alt="First slide"
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-100"
+                src={background2}
+                alt="Second slide"
+              ></img>
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-100"
+                src={background3}
+                alt="Third slide"
+              ></img>
+            </div>
+          </div>
+          <a
+            class="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
       </div>
     );
   }

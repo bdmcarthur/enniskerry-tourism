5c965bcbcc3d8b7bd0e9bf7ececf47bfd6e48c21
Brandy McArthur <bdm.mcarthur@gmail.com>
2020-01-06 15:05:10 -0500

    Preparing for deployment

---
+++ build/precache-manifest.c8c55dfa3b7a42a165be041c15654fa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e1a5918b236997318aa2543fd3d0c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe06d033c30853571656",
+    "url": "/static/css/main.10b40805.chunk.css"
+  },
+  {
+    "revision": "836b975fd0947a392f1f",
+    "url": "/static/js/2.35291324.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.35291324.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe06d033c30853571656",
+    "url": "/static/js/main.56576f66.chunk.js"
+  },
+  {
+    "revision": "42e369076200f1107223",
+    "url": "/static/js/runtime-main.dd13247d.js"
+  }
+]);
